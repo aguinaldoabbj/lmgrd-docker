a6f21911d2822543531d21e13155498d34df9671
Aguinaldo Bezerra <aguinaldoabbj@users.noreply.github.com>
2021-01-15 10:12:42 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-wget $LICENSE_URL -O $LICENSE_DIR/license
-
-if (file $LICENSE_DIR/license | grep -q Zip); 
-then
-    unzip $LICENSE_DIR/license -d $LICENSE_DIR
-    rm -vf $LICENSE_DIR/license
-else
-    mv $LICENSE_DIR/license $LICENSE_DIR/license.lic
-fi
+wget $LICENSE_URL -O $LICENSE_DIR/license.lic
 
 #sleep 300
 
